5ff3a3b5d2df80b2deee0512b972f62a1ea017a3
Odysseas Lamtzidis <odyslam@gmail.com>
2021-10-15 09:42:36 -0500

    add citydao style to tailwind, add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,15 @@ module.exports = {
   purge: ["./src/**/*.js"],
   target: "relaxed",
   theme: {
+    colors:
+    {
+      citydaoGreen: {
+        DEFAULT: '#06BE7F'
+      }
+    },
     extend: {
       fontFamily: {
-        sans: ["Inter"],
+        sans: ["Space Grotesk"],
       },
     },
   },
